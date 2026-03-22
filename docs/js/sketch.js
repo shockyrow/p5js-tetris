8dab24ae98d185fe9cb1ac3c9b5e276f710982c2
Ibrohim Shokirov <ishokirov@hotmail.com>
2026-03-23 05:04:14 +0700

    Added swipe controls support for touch screens

--- docs/js/sketch.js
+++ docs/js/sketch.js
@@ -6,6 +6,51 @@ let shapesBag = null;
 let board = null;
 let artist = null;
 
+let touchstartX = 0;
+let touchendX = 0;
+let touchstartY = 0;
+let touchendY = 0;
+
+function checkSwipe() {
+  const threshold = 16;
+  const diffX = touchendX - touchstartX;
+  const diffY = touchendY - touchstartY;
+  const vertical = Math.abs(diffX) < Math.abs(diffY);
+  const select = Math.max(Math.abs(diffX), Math.abs(diffY)) < threshold;
+
+  let move = '';
+
+  if (select) {
+    move = 'select';
+  } else if (vertical) {
+    if (diffY > 0) {
+      move = 'down';
+    } else {
+      move = 'up';
+    }
+  } else {
+    if (diffX > 0) {
+      move = 'right';
+    } else {
+      move = 'left';
+    }
+  }
+
+  makeMove(move);
+}
+
+document.addEventListener('touchstart', e => {
+  touchstartX = e.changedTouches[0].screenX;
+  touchstartY = e.changedTouches[0].screenY;
+  console.log('test');
+});
+
+document.addEventListener('touchend', e => {
+  touchendX = e.changedTouches[0].screenX;
+  touchendY = e.changedTouches[0].screenY;
+  checkSwipe();
+});
+
 function setup() {
   shapesBag = ShapesBag.random();
   board = new Board(BOARD_ROWS, BOARD_COLS);
@@ -52,13 +97,30 @@ function windowResized() {
 function mouseClicked() {
   if (!DEBUG_MODE) return;
 
-  const {x, y} = artist.getPos(mouseX, mouseY);
+  const { x, y } = artist.getPos(mouseX, mouseY);
   board.addTile(y, x);
 }
 
 function keyPressed() {
+  const keyCodeMoveMap = {
+    32: 'select',
+    [UP_ARROW]: 'up',
+    [DOWN_ARROW]: 'down',
+    [RIGHT_ARROW]: 'right',
+    [LEFT_ARROW]: 'left',
+    // [ENTER]: 'explode',
+  };
+
+  const move = keyCodeMoveMap[keyCode] ?? '';
+
+  if (move !== '') {
+    makeMove(move);
+  }
+}
+
+function makeMove(move) {
   if (board.isOver()) {
-    if (keyCode === 32) {
+    if (move === 'select') {
       board = new Board(BOARD_ROWS, BOARD_COLS);
     }
 
@@ -68,15 +130,15 @@ function keyPressed() {
   const shape = board.getShape();
 
   if (shape !== null) {
-    if (keyCode === UP_ARROW) {
+    if (move === 'up') {
       shape.nextVariant();
-    } else if (keyCode === DOWN_ARROW && board.shapeCanMoveTo(1, 0)) {
+    } else if (move === 'down' && board.shapeCanMoveTo(1, 0)) {
       shape.move(1, 0);
-    } else if (keyCode === RIGHT_ARROW && board.shapeCanMoveTo(0, 1)) {
+    } else if (move === 'right' && board.shapeCanMoveTo(0, 1)) {
       shape.move(0, 1);
-    } else if (keyCode === LEFT_ARROW && board.shapeCanMoveTo(0, -1)) {
+    } else if (move === 'left' && board.shapeCanMoveTo(0, -1)) {
       shape.move(0, -1);
-    } else if (keyCode === ENTER) {
+    } else if (move === 'explode') {
       board.explodeShape();
     }
   }
